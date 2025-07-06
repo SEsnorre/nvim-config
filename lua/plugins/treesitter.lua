6fb1a5978ebb948768fd400cf7fb47a2c1b977f5
SEsnorre <stephan.poernbacher@outlook.com>
2025-07-06 14:33:17 +0200

    adding autoinstall for treesitter, so it installs parsers automaticly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
+    branch = "master",
     lazy = false,
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "lua", "vim", "sql", "r", "typescript", "javascript", "python",
-                "html", "css", "powershell", "markdown", "markdown_inline",
-                "json", "json5", "htmldjango", "bash"
-            },
-            highlight = {enable = true},
-            indent = {enable = true},
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
         })
-    end
+    end,
 }
