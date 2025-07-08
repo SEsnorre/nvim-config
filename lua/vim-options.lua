7256fbdb2d3a52550e009443de048f3a35c11e2d
SEsnorre <stephan.poernbacher@outlook.com>
2025-07-08 13:42:33 +0200

    adding functionality for leader

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set({ "n", "v" }, "<space>", "<Nop>")
 vim.cmd("set number")
 
