a75dea7945805e15e40378648eabfeb96f7f1c73
SEsnorre <stephan.poernbacher@outlook.com>
2025-10-24 00:28:19 +0200

    lensline inline rendering

--- lua/plugins/lensline.lua
+++ lua/plugins/lensline.lua
@@ -3,6 +3,17 @@ return {
 	tag = "2.0.0", -- or: branch = 'release/2.x' for latest non-breaking updates
 	event = "LspAttach",
 	config = function()
-		require("lensline").setup()
+		require("lensline").setup({
+			profiles = {
+				{
+					name = "minimal",
+					style = {
+						placement = "inline",
+						prefix = "",
+						-- render = "focused", optionally render lenses only for focused function
+					},
+				},
+			},
+		})
 	end,
 }
